b29571fae8de06f62eb828b0f8251ddd9b61796f
unknown <rahuldevgan572@gmail.com>
2021-03-09 10:50:54 -0800

    hello

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.widget.Toast.makeText(MainActivity.this, "HEllo", Toast.LENGTH_SHORT).show();
     }
 }
